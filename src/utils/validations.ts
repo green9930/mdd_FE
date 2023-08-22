c624780966add8008e48910c3dab1c1854281f70
geuna <geuna0204@gmail.com>
2023-08-22 16:06:31 +0900

    [fix] 프로필 사진 비율 수정

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -2,7 +2,7 @@
 
 export const DISK_NAME_MAX_LENGTH = 30;
 export const DISK_IMG_MAX_LENGTH = 4;
-export const DISK_IMG_MAX_SIZE = 10000000; // 10MB = 10,000,000byte
+export const IMG_MAX_SIZE = 10000000; // 10MB = 10,000,000byte
 export const DISK_CONTENT_MAX_LENGTH = 300;
 
 export const NICKNAME_MAX_LENGTH = 10;
